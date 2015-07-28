51826f15dcc0831cc175a58b8d3493a35a4476b0
Jamie Simper <fromraintohope@gmail.com>
2015-07-28 19:02:09 -0400

    added hover effect

--- app.js
+++ app.js
@@ -19,6 +19,7 @@
       dominant = document.createElement('div');
       dominant.className = "dominant";
       dominant.style.backgroundColor = color.dominant;
+      dominant.style.color = color.dominant;
       hex = document.createElement('div');
       hex.className = "hex";
       hex.innerHTML = color.dominant;
@@ -28,6 +29,7 @@
       contrastingDominant = document.createElement('div');
       contrastingDominant.className = "contrastingDominant";
       contrastingDominant.style.backgroundColor = color.contrastingDominant;
+      contrastingDominant.style.color = color.contrastingDominant;
       hex = document.createElement('div');
       hex.className = "hex";
       hex.innerHTML = color.contrastingDominant;
@@ -37,6 +39,7 @@
       subDominant = document.createElement('div');
       subDominant.className = "subDominant";
       subDominant.style.backgroundColor = color.subDominant;
+      subDominant.style.color = color.subDominant;
       hex = document.createElement('div');
       hex.className = "hex";
       hex.innerHTML = color.subDominant;
@@ -46,6 +49,7 @@
       contrastingSubDominant = document.createElement('div');
       contrastingSubDominant.className = "contrastingSubDominant";
       contrastingSubDominant.style.backgroundColor = color.contrastingSubDominant;
+      contrastingSubDominant.style.color = color.contrastingSubDominant;
       hex = document.createElement('div');
       hex.className = "hex";
       hex.innerHTML = color.contrastingSubDominant;
@@ -55,6 +59,7 @@
       pop = document.createElement('div');
       pop.className = "pop";
       pop.style.backgroundColor = color.pop;
+      pop.style.color = color.pop;
       hex = document.createElement('div');
       hex.className = "hex";
       hex.innerHTML = color.pop;
